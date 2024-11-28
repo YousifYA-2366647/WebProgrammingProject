6f8e75ad983f602612d2cc217a21b0cc1942ee6c
Jordy Meier <jordy.meier@student.uhasselt.be>
2024-11-28 20:46:16 +0100

    calender functionaliteit verder werken

--- public/js/calender.js
+++ public/js/calender.js
@@ -1,7 +1,5 @@
-function createCalender(month, year) {
-    var date = new Date(year, month - 1, 1);
-
-    document.getElementById("datetext").textContent = date.toLocaleString('en-us', { month: 'long', year: 'numeric' });
+function calcDate(index) {
+    var date = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
 
     var ofset = date.getDay();
     if (ofset == 0) { // Date gebruikt 0 als zondag
@@ -10,46 +8,73 @@ function createCalender(month, year) {
         ofset--;
     }
 
+    if (index < ofset) {
+        return null;
+    }
+
+    date.setDate(date.getDate() + (index - ofset));
+
+    if (date.getMonth() != currentDate.getMonth()) {
+        return null;
+    }
+
+    return date;
+}
+
+
+function createCalender(month, year) {
+    // set header text
+    document.getElementById("datetext").textContent = currentDate.toLocaleString('en-us', { month: 'long', year: 'numeric' });
+
+    // set day text
     for (var i = 0; i < 40; i++) {
         var li = document.getElementById(i);
+        var date = calcDate(i);
 
-        if (i < ofset) {
+        if (!calcDate(i)) {
             li.textContent = "";
             li.className = "";
+            li.removeEventListener('click', dateClick);
             continue;
         }
 
-        if (date.getMonth() + 1 == month) {
-            if (date.getFullYear() == today.getFullYear() && date.getMonth() == today.getMonth() && date.getDate() == today.getDate()) {
-                li.className = "active";
-            } else {
-                li.className = "clickable";
-            }
+        li.className = "clickable";
 
-            li.textContent = date.getDate();
-
-            date.setDate(date.getDate() + 1);
-            continue;
+        if (date.getFullYear() == today.getFullYear() && date.getMonth() == today.getMonth() && date.getDate() == today.getDate()) {
+            li.classList.add("active");
         }
 
-        li.textContent = "";
-        li.className = "";
+        li.textContent = date.getDate();
+        li.addEventListener('click', dateClick);
     }
 }
 
-function leftClickEvent() {
+
+function dateClick(e) {
+    if (selectedIndex) {
+        document.getElementById(selectedIndex).classList.remove('clicked');
+    }
+    e.target.classList.add('clicked');
+    selectedIndex = Number(e.target.id);
+
+    selectedDate = calcDate(selectedIndex);
+    console.log(selectedDate);
+}
+
+
+// month selectors
+document.getElementById("left").addEventListener('click', () => {
     currentDate.setMonth(currentDate.getMonth() - 1);
     createCalender(currentDate.getMonth() + 1, currentDate.getFullYear());
-}
-
-function rightClickEvent() {
+});
+document.getElementById("right").addEventListener('click', () => {
     currentDate.setMonth(currentDate.getMonth() + 1);
     createCalender(currentDate.getMonth() + 1, currentDate.getFullYear());
-}
+});
 
-document.getElementById("left").onclick = leftClickEvent;
-document.getElementById("right").onclick = rightClickEvent;
 
 const today = new Date();
 var currentDate = new Date();
 createCalender(currentDate.getMonth() + 1, currentDate.getFullYear());
+var selectedIndex = null;
+var selectedDate = null;
